f3a60bc4cabb26c9fde5784945b951075fea31d4
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-16 20:26:10 +0300

    Add Exercise001_CoordinatSearch

---
+++ Exercise001_CoordinatSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿/*/Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и
+Y ≠ 0
+//и выдаёт номер четверти плоскости, в которой находится эта точка.*/
+
+Console.Clear();
+Console.Write("Введите X: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine());
+if(X > 0 && Y > 0)
+{
+    Console.WriteLine("1");
+}
+else if(X < 0 && Y > 0)
+{
+    Console.WriteLine("2");
+}
+else if(X < 0 && Y < 0)
+{
+    Console.WriteLine("3");
+}
+else if(X > 0 && Y < 0)
+{
+    Console.WriteLine("4");
+}
+else
+{
+    Console.WriteLine("Ошибка! Точка попадает на оси координат!");
+}
