38113446aec36b2834dc0d3bd21f6f2fb0cfb709
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-16 21:48:33 +0300

    Add Exercise002_CoordinatSearch

---
+++ Exercise002_CoordinatSearch/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти осей координат: ");
+int number = int.Parse(Console.ReadLine());
+
+if( number ==1)
+{
+    Console.WriteLine("X>0;Y>0");
+}
+else if(number == 2)
+{
+    Console.WriteLine("X>0;Y<0");
+}
+else if(number == 3)
+{
+    Console.WriteLine ("X<0;Y<0");
+}
+else if(number == 4)
+{
+    Console.WriteLine ("X<0;Y>0");
+}
+else Console.WriteLine ("Ошибка ввода");
