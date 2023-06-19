3b553a338b51f1aaf859f4d6369decc3efd1eb0e
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-19 22:31:45 +0300

    Add Ex03 & Ex04

---
+++ Exercise003_Distanse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите x точки А: ");
+double dotAx = double.Parse(Console.ReadLine()??"");
+Console.Write("Введите y точки A: ");
+double dotAy = double.Parse(Console.ReadLine()??"");
+Console.Write("Введите x точки B: ");
+double dotBx = double.Parse(Console.ReadLine()??"");
+Console.Write("Введите y точки B: ");
+double  dotBy = double.Parse(Console.ReadLine()??"");
+
+double distanse = Math.Sqrt((dotBx-dotAx)*(dotBx-dotAx)+(dotBy-dotAy)*(dotBy-dotAy));
+
+Console.WriteLine ($"{distanse:f2}");
---
+++ Exercise004_SquaresNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()??"");
+
+int count = 1;
+
+while (count<=number)
+{
+int Pow = count*count;
+Console.Write ($"{Pow}, ");
+
+count = count+1;
+
+
+
+}
